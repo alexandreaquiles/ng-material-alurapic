e3cda5c8b0602cd8ac3feeed9f9d9362179d4783
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-02-22 17:56:02 -0300

    capitulo 7 - removendo uma foto

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -8,4 +8,16 @@ angular
       .error(function(erro) {
         console.error('As fotos não foram carregadas')
       });
+    $scope.remover = function(foto) {
+      $http.delete('/v1/fotos/' + foto._id)
+        .success(function() {
+          var indiceDaFoto = $scope.fotos.indexOf(foto);
+          $scope.fotos.splice(indiceDaFoto, 1);
+          $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso';
+        })
+        .error(function(erro) {
+          console.log(erro);
+          $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+        });
+    }
 });
