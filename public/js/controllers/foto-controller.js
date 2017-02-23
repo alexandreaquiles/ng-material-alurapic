bddc88ef960f6b8bcbdbc5c054ed981694410970
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-02-23 16:00:45 -0300

    capitulo 9 - facilitando a comunicacao com o servidor

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,10 +1,8 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
+angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams){
   if($routeParams.fotoId) {
-    $http.get('/v1/fotos/' + $routeParams.fotoId)
-    .success(function(foto) {
+    recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
       $scope.foto = foto;
-    })
-    .error(function(erro) {
+    }, function(erro) {
       console.log(erro);
       $scope.mensagem = 'Não foi possível obter a foto'
     });
@@ -12,22 +10,18 @@ angular.module('alurapic').controller('FotoController', function($scope, $http, 
   $scope.submeter = function(){
     if($scope.formulario.$valid) {
       if($routeParams.fotoId) {
-          $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-          .success(function() {
+          recursoFoto.update({fotoId: $routeParams.fotoId}, $scope.foto, function() {
               $scope.mensagem = 'Foto alterada com sucesso';
-          })
-          .error(function(erro) {
+          }, function(erro) {
               console.log(erro);
               $scope.mensagem = 'Não foi possível alterar';
           });
       } else {
-        $http.post('/v1/fotos/', $scope.foto)
-        .success(function() {
+        recursoFoto.save($scope.foto, function() {
           $scope.mensagem = 'Foto cadastrada com sucesso';
           $scope.foto = {};
           $scope.formulario.$setPristine();
-        })
-        .error(function() {
+        }, function() {
           console.log(erro);
           $scope.mensagem = 'Não foi possível cadastrar a foto';
         });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,21 +1,17 @@
 angular
   .module('alurapic')
-  .controller('FotosController', function($scope, $http) {
-    $http.get('v1/fotos')
-      .success(function(fotos) {
+  .controller('FotosController', function($scope, recursoFoto) {
+    recursoFoto.query(function(fotos) {
         $scope.fotos = fotos;
-      })
-      .error(function(erro) {
+      }, function(erro) {
         console.error('As fotos não foram carregadas')
       });
     $scope.remover = function(foto) {
-      $http.delete('/v1/fotos/' + foto._id)
-        .success(function() {
+      recursoFoto.delete({fotoId: foto._id}, function() {
           var indiceDaFoto = $scope.fotos.indexOf(foto);
           $scope.fotos.splice(indiceDaFoto, 1);
           $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso';
-        })
-        .error(function(erro) {
+        }, function(erro) {
           console.log(erro);
           $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
         });
