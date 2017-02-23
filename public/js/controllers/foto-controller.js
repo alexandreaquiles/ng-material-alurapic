d7640c6991b39060112837c4854633ddd00b5899
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-02-23 16:17:50 -0300

    capitulo 10 - recurso de cadastro de fotos mais esperto

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,4 +1,4 @@
-angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams){
+angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos){
   if($routeParams.fotoId) {
     recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
       $scope.foto = foto;
@@ -9,23 +9,17 @@ angular.module('alurapic').controller('FotoController', function($scope, recurso
   }
   $scope.submeter = function(){
     if($scope.formulario.$valid) {
-      if($routeParams.fotoId) {
-          recursoFoto.update({fotoId: $routeParams.fotoId}, $scope.foto, function() {
-              $scope.mensagem = 'Foto alterada com sucesso';
-          }, function(erro) {
-              console.log(erro);
-              $scope.mensagem = 'Não foi possível alterar';
-          });
-      } else {
-        recursoFoto.save($scope.foto, function() {
-          $scope.mensagem = 'Foto cadastrada com sucesso';
+      cadastroDeFotos.cadastrar($scope.foto)
+      .then(function(dados) {
+        $scope.mensagem = dados.mensagem;
+        if (dados.inclusao) {
           $scope.foto = {};
           $scope.formulario.$setPristine();
-        }, function() {
-          console.log(erro);
-          $scope.mensagem = 'Não foi possível cadastrar a foto';
-        });
-      }
+        }
+      })
+      .catch(function(erro) {
+        $scope.mensagem = erro.mensagem;
+      });
     }
   }
 });
